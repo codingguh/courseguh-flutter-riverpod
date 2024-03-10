09b26663321b356e57366d0c5f4629be3019156e
codingguh <teguhharits91@gmail.com>
2024-03-10 16:09:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+final appCount = StateProvider<int>((ref) {
+  return 1;
+});
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int count = ref.watch(appCount);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Riverpod app'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$count',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              ref.read(appCount.notifier).state++;
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.arrow_right_rounded),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              ref.read(appCount.notifier).state++;
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          )
+        ],
+      ),
+    );
+  }
+}
